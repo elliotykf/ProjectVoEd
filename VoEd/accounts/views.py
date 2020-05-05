7230a749952acbe3c9c2d0e4883b9d98314c215b
elliotykf <44538316+elliotykf@users.noreply.github.com>
2020-05-05 11:31:29 -0700

    upload

--- VoEd/accounts/views.py
+++ VoEd/accounts/views.py
@@ -3,14 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from .models import AllLogin
 from django.contrib.auth import authenticate, login, logout
+import datetime
 
 
 # Create your views here.
 
+
 def gate(request):
+    #when the gate view is called, the gate.html will be displayed to users
     return render(request, 'accounts/gate.html')
 
 def homepage(request):
+    #when the homepage view is called, the home.html will be displayed to users
     return render(request, 'accounts/home.html')
 
 def registration(request):
@@ -25,17 +29,24 @@ def registration(request):
     return render(request, 'accounts/registration.html', context)
 
 def userlogin(request):
+    # timenow = datetime.datetime.now()
+    # print(timenow)
     if request.method == 'POST':
+        #get user input of the username and password
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        #authenticate the user
         user = authenticate(request, username=username, password=password)
 
+        #if the user is authenticated
         if user is not None:
+            #call login function and log the user in
             login(request, user)
+            #log the timestamp of login into the AllLogin database
             AllLogin.objects.create(user=request.user)
 
-        return redirect('homepage')
-
+        #render home.html to user by calling the homepage view
+            return redirect('homepage')
+    #if the user is not authenticated, return to the login page
     return render(request, 'accounts/userlogin.html')
 
